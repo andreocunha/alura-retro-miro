c1a7bbeb935ab3ce7494570cc4cb6359806bc554
andreocunha <andreoliveiracunha20@gmail.com>
2023-03-05 22:49:48 -0300

    server

---
+++ server/dist/utils/colors.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COLORS = void 0;
+const COLORS = [
+    "#E57373",
+    "#9575CD",
+    "#4FC3F7",
+    "#81C784",
+    "#FFF176",
+    "#FF8A65",
+    "#F06292",
+    "#7986CB",
+];
+exports.COLORS = COLORS;
