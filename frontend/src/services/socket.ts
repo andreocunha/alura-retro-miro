f015ac97496e8119c1fd4f18dc343886fc087d9a
andreocunha <andreoliveiracunha20@gmail.com>
2023-04-26 16:26:24 -0300

    server

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 // const socket = io('http://localhost:4000');
 // const socket = io('http://192.168.15.43:4000');
-// const socket = io('https://alura-retro-miro-server.herokuapp.com');
-const socket = io('https://alura-retro-miro-server.onrender.com');
+const socket = io('https://alura-retro-miro-server.herokuapp.com');
+// const socket = io('https://alura-retro-miro-server.onrender.com');
 
 export { socket };
